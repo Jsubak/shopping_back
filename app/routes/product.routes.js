7e0843762b3ef8d60d139b535e4611d84139ca7e
Jsubak <tnwjd1003@gmail.com>
2022-07-11 14:47:39 +0900

    router 세팅 및 server.js에 적용

---
+++ app/routes/product.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+    const product = require("../controllers/product.controller.js");
+    var router = require("express").Router();
+
+    // 새로운 Product 제작
+    router.post("/", product.create);
+
+    // Product 모든 리스트
+    router.get("/", product.findAll);
+
+    // Product 원하는 값 검색
+    router.get("/:id", product.findOne);
+
+    // Product colunm 업데이트
+    router.put("/:id", product.update);
+
+    // Product colunm 삭제
+    router.delete("/:id", product.delete);
+
+    app.use('/api/product', router);
+}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = requie('cors');
+const cors = require('cors');
 const app = express();
 
 let corsOptions = {
@@ -10,10 +10,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// 간단한 경로
 app.get("/", (req, res) => {
     res.json({message: "연결 성공"})
 })
 
+require("./app/routes/product.routes")(app);
+
+// 포트를 정하고 리슨에 요청
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`${PORT}에서 서버시작`);

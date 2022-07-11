ff2f62deac015be4ad76a554c4c5db0e13faafa9
Jsubak <tnwjd1003@gmail.com>
2022-07-11 14:11:07 +0900

    model파트에 등록, 목록, 특정 값 검색, 삭제 제작

--- app/models/product.model.js
+++ app/models/product.model.js
@@ -5,3 +5,87 @@ const Product = function(product) {
     this.productdes = product.productdes;
     this.productimg = product.productimg;
 }
+
+Product.create = (newProduct, result) => {
+    sql.query("INSERT INTO product SET ?", newProduct, (err, res) => {
+        if(err) {
+            console.log("error: ", err);
+            result(err.null);
+            return;
+        }
+        console.log("created product: ", {id: res.insertId, ...newProduct});
+        result(null, {id: res.insertId, ...newProduct});
+    })
+}
+
+Product.findById = (id, result) => {
+    sql.query(`SELECT * FROM product WHERE productid = ${id}`, (err, res) => {
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+            return
+        }
+        if(res.length) {
+            console.log("found product: ", res[0])
+            return
+        }
+        // id를 못찾을 때
+        result({kind: "not_found"}, null);
+    })
+}
+
+Product.getAll = (name, result) => {
+    let query = "SELECT * FROM product ";
+    if (name) {
+        query += `WHERE productname LIKE '%${name}%'`
+    }
+    sql.query(query, (err, res) => {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("product: ", res);
+        result(null, res);
+    })
+}
+
+Product.updateById = (id, product, result) => {
+    sql.query(
+        "UPDATE product SET productname = ?, productdes = ?, productimg = ? WHERE productid = ?",
+        [product.productname, product.productdes, product.productimg, id],
+        (err, res) => {
+            if(err) {
+                console.log("error: ", err);
+                result(null, err);
+                return;
+            }
+            if(res.affectedRows == 0) {
+                result({kind: "not_found"}, null);
+                return;
+            }
+            console.log("updated product: ", {id: id, ...product})
+            result(null, {id: id, ...product})
+        }
+    )
+}
+
+Product.remove = (id, result) => {
+    sql.query("DELETE FROM product WHERE productid = ?", id, (err, res) => {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+
+        if(res.affectedRows == 0) {
+            result({kind: "not_found"}, null);
+            return;
+        }
+
+        console.log("deleted product with id: ", id);
+        result(null, res);
+    });
+}
+
+module.exports = Product;
